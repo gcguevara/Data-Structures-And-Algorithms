4857fb7f406658363b0759c0dcc3ef6a8c31c73b
gcguevara <garrettcguevara@gmail.com>
2017-02-28 16:16:19 -0800

    Add methods CreateList, DisplayList CountNodes, and Search.

---
+++ SingleLinkedList/SingleLinkedList/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice, data, k, x;
+
+            SingleLinkedList list = new SingleLinkedList();
+
+            list.CreateList();
+
+            while (true)
+            {
+                Console.WriteLine("1. Display list.");
+                Console.WriteLine("2. Count the number of nodes.");
+                Console.WriteLine("3. Search for an element.");
+                Console.WriteLine("4. Insert in empty list/ insert in beginning of the linked list.");
+                Console.WriteLine("5. Insert a node at the end of the list.");
+                Console.WriteLine("6. Insert a node after a specificied node.");
+                Console.WriteLine("7. Insert a node before a specified node.");
+                Console.WriteLine("8. Insert a node at a given position.");
+                Console.WriteLine("9. Delete a first node.");
+                Console.WriteLine("10. Delete last node.");
+                Console.WriteLine("11. Delete any node.");
+                Console.WriteLine("12. Reverse the list.");
+                Console.WriteLine("13. Bubble sort by exchanging data.");
+                Console.WriteLine("14. Bubble sort by exchanging links.");
+                Console.WriteLine("15. MergeSort.");
+                Console.WriteLine("16. Insert cycle.");
+                Console.WriteLine("17. Detect cycle.");
+                Console.WriteLine("18. Remove cycle.");
+                Console.WriteLine("19. Quit.");
+
+                Console.WriteLine("");
+                Console.WriteLine("Enter your choice : ");
+                choice = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("");
+
+                if (choice == 19)
+                {
+                    break;
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        list.DisplayList();
+                        break;
+                    case 2:
+                        list.CountNodes();
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter the element to be searched : ");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        list.Search(data);
+                        break;
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ SingleLinkedList/SingleLinkedList/SingleLinkedList.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleLinkedList
+{
+    class SingleLinkedList
+    {
+        private Node start;
+
+        public SingleLinkedList()
+        {
+            start = null;
+        }
+
+        public void CreateList()
+        {
+            Console.WriteLine("Enter the number of nodes : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine("Enter the element to be inserted : ");
+                int data = Convert.ToInt32(Console.ReadLine());
+                InsertAtEnd(data);
+            }
+
+        }
+
+        public void DisplayList()
+        {
+            Node p;
+            if (start == null)
+            {
+                Console.WriteLine("List is empty.");
+                return;
+            }
+            Console.WriteLine("List is : ");
+            p = start;
+            while (p != null)
+            {
+                Console.WriteLine(p.info + " ");
+                p = p.link;
+            }
+            Console.WriteLine("End of list.");
+        }
+
+        public void CountNodes()
+        {
+            int n = 0;
+            Node p = start;
+            while (p != null)
+            {
+                n++;
+                p = p.link;
+            }
+            Console.WriteLine("Number of nodes in the list = " + n);
+        }
+
+        public bool Search(int x)
+        {
+            int position = 1;
+            Node p = start;
+            
+            while (p != null)
+            {
+                if (p.info == x)
+                {
+                    break;
+                }
+                position++;
+                p = p.link;
+            }
+            if (p == null)
+            {
+                Console.WriteLine(x + " was not found in the list.");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine(x + " is at position " + position);
+                return true;
+            }
+        }
+
+        public void InsertAtEnd(int data)
+        {
+            Node p;
+            Node temp = new Node(data);
+
+            if (start == null)
+            {
+                start = temp;
+                return;
+            }
+
+            p = start;
+            while (p.link != null)
+            {
+                p = p.link;
+            }
+
+            p.link = temp;
+        }
+    }
+}
